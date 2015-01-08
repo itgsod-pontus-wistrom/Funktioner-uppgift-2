8dcbb266c3a648d04a28ef98cdbd12962949aa2d
itgsod-pontus-wistrom <pontus.wistrom@elev.it-gymnasiet.se>
2015-01-08 10:22:15 +0100

    uppgift2e
    uppgift

--- uppgift2e.rb
+++ uppgift2e.rb
@@ -2,5 +2,5 @@ def grader(t)
   return 96*0.96**t
 end
 puts "Hur många timmar har gått?"
-temp=gets.to_f
-puts grader(temp)
+timmar=gets.to_f
+puts "Kaffet är #{grader(timmar)} grader effter #{timmar} timmar"
